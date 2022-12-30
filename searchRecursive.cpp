1b8164d4a291cdc9d330c59624309a00bb1d8fef
pratapshingane18 <sushrutrao465@gmail.com>
2022-12-30 16:59:20 +0530

    Search

---
+++ searchRecursive.cpp
@@ -0,0 +1,162 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+
+}*first = NULL;
+
+void create(int A[], int n){
+    int i;
+    struct Node *t, *last;
+    first= new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last=first;
+
+    for(int i =1; i <n; i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last =t;
+
+        
+    }
+}
+
+
+void display(struct Node *p){
+    while(p!= NULL){
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+void RecursiveDisplay( struct Node *p){
+    if(p!=0){
+        cout<<p->data<<endl;
+        RecursiveDisplay(p->next);
+    }
+}
+
+void ReverseRecursiveDisplay( struct Node *p){
+    if(p!=0){
+        
+        ReverseRecursiveDisplay(p->next);
+        cout<<p->data<<endl;
+    }
+}
+
+int countNode(struct Node *p){
+    int c=0;
+
+    while(p!=0){
+        c++;
+        p = p->next;
+
+    }
+
+    return c;
+}
+
+int countRecursive(struct Node *p){
+
+    if(p==0){
+        return 0;
+    }
+    else{
+        return 1 + countRecursive(p->next) ;
+    }
+}
+
+int sum(struct Node *p){
+
+    int sum =0;
+    while(p!=0){
+        sum += p->data;
+        p = p->next;
+    }
+    return sum;
+}
+
+int sumRecurrsive(struct Node *p){
+    if(p ==0){
+        return 0;
+
+    }
+
+    else{
+        return sumRecurrsive(p->next) + p->data;
+    }
+}
+
+int MaximumElement(struct Node *p){
+    int CurrMax = -32768;
+    while(p!=0){
+        if(p->data > CurrMax){
+            CurrMax = p->data;
+            p = p->next;
+        }
+    }
+
+    return CurrMax;
+}
+
+int MaxEleRecursive(struct Node* p){
+    int x =0;
+    if(p ==0){
+        return -32768;
+    }
+
+    else{
+        x = MaxEleRecursive(p->next);
+
+        // if(x > p->data){
+        //     return x;
+        // }
+
+        // else{
+        //     return p->data;
+        // }
+
+        return x>p->data?x:p->data;
+    }
+}
+
+Node* Search(struct Node *p, int Key){
+
+    while(p!=0){
+        if(Key == p->data ){
+            cout<<"Element "<<p->data<<" is present in the LL"<<endl;
+
+        }
+        
+        p=p->next;
+
+    }
+
+    return NULL;
+}
+
+
+Node* searchRecursive(struct Node *p, int key){
+    if(p == NULL){
+        return NULL;
+    }
+
+    if(key == p->data){
+        return p;
+    }
+
+    return searchRecursive(p->next, key);
+}
+int main(){
+    int A[] ={1,2,3,4,5,6};
+    create(A,6);
+    // ReverseRecursiveDisplay(first);
+    cout<<searchRecursive(first,5);
+    return 0;
+}
+
