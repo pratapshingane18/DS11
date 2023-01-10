3a1f79c7040d49d5dcd863cc7e7dc64f8d7c9acb
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-10 09:45:11 +0530

    doublyLL delete

---
+++ DeleteDLL.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+
+}*first = NULL;
+
+void create(int A[], int n){
+    struct Node *t, *last;
+    int i;
+
+    first = new Node;
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+
+    last = first;
+
+    for(i =1; i <n; i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        
+        last->next = t;
+        last =t;
+
+
+    }
+
+
+}
+
+void display(struct Node *p){
+    while(p){
+        cout<<p->data;
+        cout<<" ";
+        p = p->next;
+    }
+    cout<<endl;
+}
+
+int length(struct Node *p){
+    int length = 0;
+     while(p){
+        length += p->data;
+        p = p->next;
+    }
+    cout<<endl;
+
+    return length;
+}
+
+void insert(struct Node *p, int index, int x){
+    // int k=1;
+    // struct Node *head = first;
+    // p = head;
+    struct Node *temp = new Node;
+
+    if(index < 0 || index > length(p)){
+        return;
+    }
+
+    temp->prev = NULL;
+    temp->data =x;
+    temp->next = NULL;
+
+    if(index ==0){
+        temp->next = first;
+        first->prev =temp;
+        first =temp;
+
+    }
+
+    else{
+        for(int i =0; i <index -1; i++){
+            p = p->next;
+        }
+        temp->prev = p;
+        temp->next = p->next;
+        
+        if(p->next){
+            p->next->prev = temp;
+            
+        }
+        p->next = temp;
+    }
+
+}
+
+void Delete(struct Node *p, int index){
+    int x;
+    if(index < 0 || index > length(p)){
+        return;
+    }
+    if(index == 0){
+        first = first->next;
+        free(first);
+        
+    }
+
+    else{
+        for(int i =0; i < index; i++){
+            p = p->next;
+        }
+
+        x = p->data;
+        p->next->prev = p->prev;
+        p->prev->next = p->next;
+        free(p);
+    }
+}
+
+int main(){
+    int A[] = {10,2,5,7,1};
+    create(A,5);
+    Delete(first,0);
+
+    display(first);
+    return 0;
+}
